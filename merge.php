2daf104edd6f441690760d4e9dd5eb827c3e4bbe
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-07-19 23:52:41 -0500

    aa

---
+++ merge.php
@@ -0,0 +1,22 @@
+<?php
+$file = "statics/results.json";
+$fileok = "statics/results_ok.json";
+$inp = file_get_contents($file);
+$json_array = json_decode($inp, TRUE);
+
+$new_array = array();
+$exists    = array();
+foreach( $json_array as $element ) {
+    if( !in_array( $element['title'], $exists )) {
+        $new_array[] = $element;
+        $exists[]    = $element['title'];
+    }
+}
+
+$jsonData = json_encode($new_array);
+$status = file_put_contents($fileok, $jsonData);
+if($status){
+	echo "listo!";
+}
+
+?>
